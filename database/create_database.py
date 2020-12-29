6904b9f6d3f46c7a8c113e437d716e48800e000b
Shreya Dubey <shreyadubey180.com>
2020-12-30 02:02:56 +0530

    almost done with backend part

--- database/create_database.py
+++ database/create_database.py
@@ -3,18 +3,34 @@ conn = sqlite3.connect('customers.sqlite')
 cursor = conn.cursor()
 print("Opened database successfully")
 
-# cursor.execute(''' create table customer
-#         (Id BLOB primary key not null,
-#         Name text,
-#         Entry_time text,
-#         Entry_happiness real,
-#         Exit_happiness real,
-#         Exit_time text);''')
-# cursor.close()
+# _____________________ Create Table _____________________________________
+cursor.execute(''' create table customer
+        (Id BLOB primary key not null,
+        Name text,
+        Entry_time text,
+        Entry_angry INT,
+        Entry_disgust INT,
+        Entry_fear INT,
+        Entry_happy INT,
+        Entry_sad INT,
+        Entry_surprise INT,
+        Entry_neutral INT,
+        Entry_cnt INT,
+        Entry_mood INT,
+        Exit_angry INT,
+        Exit_disgust INT,
+        Exit_fear INT,
+        Exit_happy INT,
+        Exit_sad INT,
+        Exit_surprise INT,
+        Exit_neutral INT,
+        Exit_time text,
+        Exit_cnt INT,
+        Result INT,
+        Exit_mood INT);''')
+cursor.close()
+# _______________ comment this secotion if table is already created _______
 
-val = 1.0
-cursor = conn.cursor()
-conn.execute(''' insert into customer (Id,entry_happiness) values (?,?)''',('1',val))
-cursor.close
 conn.commit()
 conn.close()
+
